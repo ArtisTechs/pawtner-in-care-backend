c2c46017a71c5a35d6d4b0385e7183d18bcdf8ff
ArtisTechs <suarezestanislaojose@gmail.com>
2026-03-21 21:47:57 +0800

    initial

---
+++ src/main/java/pawtner_core/pawtner_care_api/PawtnerCareApiApplication.java
@@ -0,0 +1,13 @@
+package pawtner_core.pawtner_care_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawtnerCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawtnerCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pawtner_core/pawtner_care_api/PawtnerCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pawtner_core.pawtner_care_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawtnerCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
